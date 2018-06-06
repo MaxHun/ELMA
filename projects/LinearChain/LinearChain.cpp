93ff5417ba2ee052dcd868db68a017a2fb92f944
prename surname <huneshagen@morwen.loc.ipfdd.de>
2018-06-06 15:31:09 +0200

    Added project 'Linear Chain', including LinearChain.cpp and CalcR2.cpp.

--- projects/LinearChain/LinearChain.cpp
+++ projects/LinearChain/LinearChain.cpp
@@ -26,12 +26,8 @@ along with LeMonADE.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------*/
 
 /* *********************************************************************
-* This example demonstrates the basic use of updaters and analyzers.
-* A simple simulation program is put together and can be executed.  
-* In the example, a short polymer chain is placed in the system and 
-* simulated in a periodic box for 1E7 Monte Carlo steps. The 
-* configurations are saved to a file so they can be watched later, and 
-* the radius of gyration is analyzed.
+* The aim of this program is to simulate a linear chain in a periodic box 
+* and to produce a .bfm-File that can be used for further analysis.
 * *********************************************************************/
 
 #include <LeMonADE/core/Ingredients.h>
@@ -42,18 +38,15 @@ along with LeMonADE.  If not, see <http://www.gnu.org/licenses/>.
 #include <LeMonADE/utility/TaskManager.h>
 #include <LeMonADE/updater/UpdaterSimpleSimulator.h>
 #include <LeMonADE/analyzer/AnalyzerWriteBfmFile.h>
-#include <LeMonADE/analyzer/AnalyzerRadiusOfGyration.h>
 #include <LeMonADE/updater/moves/MoveLocalSc.h>
 
+// New with respect to Example4:
+#include <iostream>
+#include <string>
+#include <sstream>
 
 int main(int argc, char* argv[]){
 	
-    /* ****************************************************************
-      * as described in the previous examples, we quickly seed the 
-      * random number generator, and set up a system in a box with the 
-      * standard BFM bondset. We add 10 monomers and connect them to a 
-      * linear polymer.
-      * ***************************************************************/
 
     //first set up the random number generator
     RandomNumberGenerators randomNumbers;
@@ -79,21 +72,21 @@ int main(int argc, char* argv[]){
     mySystem.setPeriodicZ(true);
     
     mySystem.modifyBondset().addBFMclassicBondset();
-    
+
     mySystem.synchronize(mySystem);
     
     
-    //and add a polymer chain of length ten
-    uint32_t polymerLength=10;
+    //and add a polymer chain of length N
+    uint32_t N=10;
     
-    mySystem.modifyMolecules().resize(polymerLength);
+    mySystem.modifyMolecules().resize(N);
     
     //add the polymer chain
     mySystem.modifyMolecules()[0].setX(0);
     mySystem.modifyMolecules()[0].setY(0);
     mySystem.modifyMolecules()[0].setZ(0);
     
-    for(uint32_t i=1;i<polymerLength;i++){
+    for(uint32_t i=1;i<N;i++){
 	    mySystem.modifyMolecules()[i].setX(i*2);
 	    mySystem.modifyMolecules()[i].setY(i*2);	
 	    mySystem.modifyMolecules()[i].setZ(i);
@@ -106,28 +99,13 @@ int main(int argc, char* argv[]){
     
     /* ****************************************************************
       * Now we can set up the task manager with the desired tasks.
-      * We want to do three things:
+      * We want to do two things:
       * 1) Simulate the system. For this we use the updater 
       * UpdaterSimpleSimulator. The source code of this updater
       * can be found in src/updater/UpdaterSimpleSimulator.h
       * 2) Write the trajectory to a file. For this we use the analyzer 
       * AnalyzerWriteBfmFile. The source code of this analyzer can be
       * found in src/analyzer/AnalyzerWriteBfmFile.h
-      * 3) Calculate the radius of gyration. For this we use the 
-      * analyzer AnalyzerRadiusOfGyration, which  can be found in
-      * src/analyzer/AnalyzerRadiusOfGyration.h
-      * 
-      * For adding updaters and analyzers, the task manager provides 
-      * the functions addUpdater, addAnalyzer. These functions take two
-      * arguments:
-      * 1) a pointer to the updater/analyzer
-      * 2) a number indicating how often the particular task should be 
-      * executed. For example, 1 means every circle, 2 every second 
-      * circle, etc. Setting the second argument to 0 means the task 
-      * will be executed only once in the first circle. This is useful
-      * if for example a polymer is added in the beginning by an updater.
-      * Here, we have created the polymer by hand, so this is not 
-      * necessary.
       * ***************************************************************/
     
     //create the task manager
@@ -135,46 +113,25 @@ int main(int argc, char* argv[]){
     
     //add the simulator
     //the syntax says: the simulator should simulate mySystem
-    //every time it is exeted, it simulates for 10000 steps
+    //every time it is executed, it simulates for 10000 steps
     //The 1 as second argument to addUpdater says that the 
     //simulation is to be called in every circle.
     taskmanager.addUpdater(new 
-    UpdaterSimpleSimulator<MyIngredients,MoveLocalSc>(mySystem,10000),1);
+    UpdaterSimpleSimulator<MyIngredients,MoveLocalSc>(mySystem,50000),1);
     
-    //add the file output, the trajectory file name will be "polymer.bfm"
+    //add the file output, the trajectory file name will be 
+    // "LinearChain_<ChainLength>.bfm"
+    std::ostringstream filename;
+    filename << "LinearChain_" << N << ".bfm"; 
+    
+    
     taskmanager.addAnalyzer(new 
-    AnalyzerWriteBfmFile<MyIngredients>("polymer.bfm",mySystem),1);
+    AnalyzerWriteBfmFile<MyIngredients>(filename.str(),mySystem),1);
     
-    //add the radius of gyration calculation. we execute this only 
-    //every 10th time
-    //the first argument to the analyzer is the system to be analyzed,
-    //the second is a string defining the name of the output file.
-    taskmanager.addAnalyzer(new 
-    AnalyzerRadiusOfGyration<MyIngredients>(mySystem,"Rg2.dat"),10);
     
-    /* ****************************************************************
-      * For running the desired tasks, the taskmanager provides four 
-      * functions:
-      * - initialize() : should always be called at the beginning. It 
-      * initializes the updaters and analyzers. More on this topic in 
-      * the next example of the tutorial.
-      * - run() : runs the circle of chosen tasks until all updaters 
-      * are finished
-      * - run(nCircles) : runs the circle of chosen tasks nCircles times
-      * - cleanup() : should be called at the end. finishes the tasks 
-      * of updaters and analyzers. More on this topic in the next 
-      * example of the tutorial.
-      * ***************************************************************/
-
-    //this will prepare and run the simulation. look in the directory
-    //where you called the program for output files. 
-    //we run the taskmanager 1000 times, because in every run, the
-    //simulator makes 10000 steps->total of 1E7 steps.
-    //when you run this example program, you will see some output
-    //indicating the progress of the simulation on the sceen.
     
     taskmanager.initialize();
-    taskmanager.run(1000);
+    taskmanager.run(2000);
     taskmanager.cleanup();
     
     /* **************************************************************
